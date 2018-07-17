89f6abf1cf399bccd206b3e6b7c9bf5f7d0e260e
SharifCoding <ocarinaoftime81@gmail.com>
2018-07-17 21:11:59 +0100

    setInterval timer to refresh progress bar

--- src/components/Rewards/index.jsx
+++ src/components/Rewards/index.jsx
@@ -6,21 +6,32 @@ import Reward from '../Reward';
 import './style.scss';
 
 class Rewards extends Component {
-
   constructor() {
     super();
     this.state = {
       rewards: [],
+      test: [],
     };
   }
   componentDidMount() {
     axios.get('http://127.0.0.1:3000/transaction')
     .then((response) => {
       this.setState({ rewards: response.data });
-      console.log('merchantID: ', this.state.rewards);
     })
     .catch(error => console.log(error))
+    this.timer = setInterval(() =>  this.setState(prevState => ({ test: !prevState.test })), 1000);
   }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  // renderDiv() {
+  //   if(this.state.test) {
+  //     return (<div>test is true</div>)
+  //   }
+  //   else {
+  //     return (<div>test is false</div>)
+  //   }
+  // }
   render() {
     const {
         rewards,
@@ -29,6 +40,9 @@ class Rewards extends Component {
       <div className="App">
         <Account />
         <div className="rewards__container">
+          {/* refreshes rewards container every 1 second */}
+          { this.state.test }
+          {/* { this.renderDiv() } */}
         <ul className="rewards rewards__list">
         {
           rewards.map(reward => (
